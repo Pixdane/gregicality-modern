d860d806cabfe07a7796edff40ad19c09096b262
Pixdane <yuanjin233@gmail.com>
2026-07-10 19:46:04 +0800

    chore: limit client setup class visibility

--- src/main/scala/com/pixdane/gregicality/client/Client.scala
+++ src/main/scala/com/pixdane/gregicality/client/Client.scala
@@ -5,7 +5,7 @@ import net.minecraftforge.eventbus.api.{IEventBus, SubscribeEvent}
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import org.apache.logging.log4j.Logger
 
-class Client(private val logger: Logger):
+private class Client(private val logger: Logger):
   @SubscribeEvent
   def onClientSetup(event: FMLClientSetupEvent): Unit =
     logger.info("HELLO FROM CLIENT SETUP")
