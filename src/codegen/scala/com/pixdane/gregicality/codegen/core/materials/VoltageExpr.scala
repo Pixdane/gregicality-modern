138b0a56136c3d06fcbb1388c459b1a54fb73e17
Pixdane <yuanjin233@gmail.com>
2026-07-19 13:49:06 +0800

    chore: remove codegen and symbolgen pipelines

--- src/codegen/scala/com/pixdane/gregicality/codegen/core/materials/VoltageExpr.scala
+++
@@ -1,9 +0,0 @@
-package com.pixdane.gregicality.codegen.core.materials
-
-/** Authored voltage expression. Named forms preserve whether generated source
-  * should use GTValues.V or GTValues.VA instead of eagerly computing a number.
-  */
-enum VoltageExpr:
-  case Tier(name: ScalaIdent)
-  case VA(name: ScalaIdent)
-  case Literal(value: Voltage)
